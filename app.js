a4943d7bcb68178a040ad724bd1254a92067400b
veritasx <aaronendsley@gmail.com>
2015-11-25 14:29:48 -0600

    enable the dropdown functionality

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var navElement=document.getElementById('documentNav'),
   menuButton=document.getElementById('menuToggleClose'),
   unorderedList=document.getElementById('navUl'),
-  linkTag=document.getElementsByTagName('a'),
-  showTheMenu=unorderedList.style.display='block',
-  hideTheMenu=unorderedList.style.display='none';
+  unorderedList2=document.getElementById('navUl2'),
+  linkTag=document.getElementsByTagName('a');
 
-  menuButton.onclick=function navToggle(){
-    
-  }
+
+  menuButton.addEventListener('click', function (){
+      if (unorderedList){
+        unorderedList.style.display='block';
+        unorderedList.id='navUl2';
+      }
+      if(unorderedList2){
+        unorderedList2.style.display='none';
+        unorderedList2.id='navUl';
+      }
+  });
