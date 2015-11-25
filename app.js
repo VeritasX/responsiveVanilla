67a8660cf11c248da9d52e2cb507468224be2e48
veritasx <aaronendsley@gmail.com>
2015-11-25 11:35:10 -0600

    created a new function and made changes to allow the menu button to be clicked and toggle the display element of the ul element

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 var navElement=document.getElementById('documentNav'),
   menuButton=document.getElementById('menuToggleClose'),
   unorderedList=document.getElementById('navUl'),
-  linkTag=document.getElementByTagName('a');
+  linkTag=document.getElementsByTagName('a'),
+  showTheMenu=unorderedList.style.display='block',
+  hideTheMenu=unorderedList.style.display='none';
 
-
-    function showTheMenu(){
-        unorderedList.style.display='block';
-    }
-
-    function hideTheMenu(){
-        unorderedList.style.display='none';
-    }
-
-    function navClick(){
-        
-    }
+  menuButton.onclick=function navToggle(){
+    
+  }
