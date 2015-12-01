ddd83d0db212f3971c1a1d12a64654e6f95ad692
veritasx <aaronendsley@gmail.com>
2015-12-01 12:42:37 -0600

    made the static css and html

--- carousel.js
+++ carousel.js
@@ -1,1 +1,10 @@
 //Create a carousel that can cycle through a group of divs in an HTML docucument..once it has cycled all the way through restart the process
+
+
+//Get the slideShow div
+    var slideShow=document.querySelectorAll('.inside')
+
+//cycle through the elements in the slideShow div
+    //show each element for 10 secoonds
+    //move to the next element in the div and make the previous element hide.
+    //restart the loop
