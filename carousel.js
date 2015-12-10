9337f49f5e4956d7e93b67d293fff73966d7b9ef
veritasx <aaronendsley@gmail.com>
2015-12-10 15:18:44 -0600

    erased the previous code and started over

--- carousel.js
+++ carousel.js
@@ -1,34 +1,11 @@
 'use strict';
-
-//Create a carousel that can cycle through a group of divs in an HTML docucument..once it has cycled all the way through restart the process
-
-
-
-
-
-//Get the slideShow div
-    var slideShow=document.getElementById('inside');
-    var inner=slideShow.children;
-        function inlineBlock(x){
-          setTimeout(function(){
-            inner[x].style.display='inline-block'
-          }, 4000);
-
-        }
-
-        function displayNone(x){
-          setTimeout(function(){
-            inner[x].style.display='none'
-          }, 4000);
-        }
-
-          
+//What items need to be accessed and stored to be used?
+var slideShow = document.getElementById('#slideShow');
+var inside = document.getElementById('#inside');
+var inner = document.getElementById('#inner1');
+var innerTwo = document.getElementById('#inner2');
+var innerThree = document.getElementById('#inner3');
+var innerFour = document.getElementById('#inner4');
 
 
-
-//cycle through the elements in the slideShow div
-
-      //show each element for 10 secoonds
-
-      //move to the next element in the div and make the previous element hide.
-      //restart the loop
+//G
